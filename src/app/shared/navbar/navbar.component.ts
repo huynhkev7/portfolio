d3f8776f75658d23c6f03ccd1eca14599cc9341a
Kevin Huynh <huynhkev@Kevins-MacBook-Pro.local>
2018-11-26 19:38:34 -0800

    adding navbar logic

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+declare var jQuery: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -15,6 +15,69 @@ export class NavbarComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.smoothScrolling();
+  }
+
+  // showBlackBar(): boolean {
+  //   const excludes = ['/', '/products', '/reviews', '/splash'];
+  //   return !_.includes(excludes, this.router.url);
+  // }
+
+  // isActive(currentNav: string): boolean {
+  //   if (_.isEqual(this.router.url, '')) {
+  //     return true;
+  //   }
+
+  //   return _.isEqual(this.router.url, currentNav);
+  // }
+
+  smoothScrolling(): void {
+    // Smooth scrolling using jQuery easing
+    jQuery('a.js-scroll-trigger[href*=\'#\']:not([href=\'#\'])').click(function () {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        let target = jQuery(this.hash);
+        target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          jQuery('html, body').animate({
+            scrollTop: (target.offset().top - 54)
+          }, 1000, 'easeInOutExpo');
+          return false;
+        }
+      }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    jQuery('.js-scroll-trigger').click(function () {
+      jQuery('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    jQuery('body').scrollspy({
+      target: '#mainNav',
+      offset: 56
+    });
+
+    // Collapse Navbar
+    const navbarCollapse = function () {
+      const mainNav = jQuery('#mainNav');
+      if (mainNav.offset().top > 100) {
+        mainNav.addClass('navbar-shrink');
+      } else {
+        mainNav.removeClass('navbar-shrink');
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    jQuery(window).scroll(navbarCollapse);
+
+    // Hide navbar when modals trigger
+    jQuery('.portfolio-modal').on('show.bs.modal', function (e) {
+      jQuery('.navbar').addClass('d-none');
+    });
+    jQuery('.portfolio-modal').on('hidden.bs.modal', function (e) {
+      jQuery('.navbar').removeClass('d-none');
+    });
   }
 
   private hello(): String {
