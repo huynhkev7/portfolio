a9e99aba8166d75179a7240b847ba5698a55fea5
Kevin Huynh <huynhkev@Kevins-MacBook-Pro.local>
2018-12-18 15:06:17 -0800

    adding in tech icons

--- src/app/pages/sales-express/sales-express.component.ts
+++ src/app/pages/sales-express/sales-express.component.ts
@@ -44,26 +44,26 @@ export class SalesExpressComponent implements OnInit {
     new Image(
       0,
       { // modal
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/v4/examples/systemjs/assets/images/gallery/img1.jpg',
+        img: '/assets/img/sales_express/geolink.png',
         extUrl: 'http://www.google.com'
       }
     ),
     new Image(
       1,
       { // modal
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/v4/examples/systemjs/assets/images/gallery/img2.png',
+        img: './assets/img/sales_express/anira.png',
         description: 'Description 2'
       }
     ),
     new Image(
       2,
       { // modal
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/v4/examples/systemjs/assets/images/gallery/img3.jpg',
+        img: '/assets/img/sales_express/leads.png',
         description: 'Description 3',
         extUrl: 'http://www.google.com'
       },
       { // plain
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/v4/examples/systemjs/assets/images/gallery/thumbs/img3.png',
+        img: '/assets/img/sales_express/geolink.png',
         title: 'custom title 2',
         alt: 'custom alt 2',
         ariaLabel: 'arial label 2'
@@ -72,7 +72,7 @@ export class SalesExpressComponent implements OnInit {
     new Image(
       3,
       { // modal
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/v4/examples/systemjs/assets/images/gallery/img4.jpg',
+        img: '/assets/img/sales_express/erbi_1.png',
         description: 'Description 4',
         extUrl: 'http://www.google.com'
       }
@@ -80,10 +80,7 @@ export class SalesExpressComponent implements OnInit {
     new Image(
       4,
       { // modal
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/v4/examples/systemjs/assets/images/gallery/img5.jpg'
-      },
-      { // plain
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/v4/examples/systemjs/assets/images/gallery/thumbs/img5.jpg'
+        img: '/assets/img/sales_express/user_admin_1.png'
       }
     )
   ];
