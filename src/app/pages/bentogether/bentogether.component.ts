5707d3ce4bb9cacee9ab8086dde14b6d1e0879e5
Kevin Huynh <huynhkev@Kevins-MacBook-Pro.local>
2018-12-18 16:19:24 -0800

    adding food2020 content

--- src/app/pages/bentogether/bentogether.component.ts
+++ src/app/pages/bentogether/bentogether.component.ts
@@ -44,26 +44,26 @@ export class BentogetherComponent implements OnInit {
     new Image(
       0,
       { // modal
-        img: '/assets/img/sales_express/geolink.png',
+        img: '/assets/img/bentogether/build.png',
         extUrl: 'http://www.google.com'
       }
     ),
     new Image(
       1,
       { // modal
-        img: './assets/img/sales_express/anira.png',
+        img: './assets/img/bentogether/stats.png',
         description: 'Description 2'
       }
     ),
     new Image(
       2,
       { // modal
-        img: '/assets/img/sales_express/leads.png',
+        img: './assets/img/bentogether/custom.png',
         description: 'Description 3',
         extUrl: 'http://www.google.com'
       },
       { // plain
-        img: '/assets/img/sales_express/geolink.png',
+        img: './assets/img/bentogether/login.png',
         title: 'custom title 2',
         alt: 'custom alt 2',
         ariaLabel: 'arial label 2'
@@ -72,7 +72,7 @@ export class BentogetherComponent implements OnInit {
     new Image(
       3,
       { // modal
-        img: '/assets/img/sales_express/erbi_1.png',
+        img: './assets/img/bentogether/pet.png',
         description: 'Description 4',
         extUrl: 'http://www.google.com'
       }
--- src/app/pages/food2020/food2020.component.ts
+++ src/app/pages/food2020/food2020.component.ts
@@ -45,26 +45,26 @@ export class Food2020Component implements OnInit {
     new Image(
       0,
       { // modal
-        img: '/assets/img/sales_express/geolink.png',
+        img: './assets/img/food2020/food2020_1.png',
         extUrl: 'http://www.google.com'
       }
     ),
     new Image(
       1,
       { // modal
-        img: './assets/img/sales_express/anira.png',
+        img: './assets/img/food2020/food_2020_2.png',
         description: 'Description 2'
       }
     ),
     new Image(
       2,
       { // modal
-        img: '/assets/img/sales_express/leads.png',
+        img: './assets/img/food2020/food_2020_3.png',
         description: 'Description 3',
         extUrl: 'http://www.google.com'
       },
       { // plain
-        img: '/assets/img/sales_express/geolink.png',
+        img: './assets/img/food2020/food_2020_4.png',
         title: 'custom title 2',
         alt: 'custom alt 2',
         ariaLabel: 'arial label 2'
@@ -73,7 +73,7 @@ export class Food2020Component implements OnInit {
     new Image(
       3,
       { // modal
-        img: '/assets/img/sales_express/erbi_1.png',
+        img: './assets/img/food2020/food_2020_6.png',
         description: 'Description 4',
         extUrl: 'http://www.google.com'
       }
